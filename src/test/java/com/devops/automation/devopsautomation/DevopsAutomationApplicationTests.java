85bb0071688972c0ef537110a689e8595cd01d7e
Mohd Adnan Parvez <adnansb30@gmail.com>
2024-03-21 13:25:38 +0530

    first commit

---
+++ src/test/java/com/devops/automation/devopsautomation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.automation.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
